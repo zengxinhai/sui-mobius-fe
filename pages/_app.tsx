0836d290ebd030f8eab92df184eb068dff2fd04b
Shawn <shawn@karboncard.com>
2023-01-12 13:42:41 +0800

    Add wallet support

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,11 @@
-import '../styles/globals.css'
+import '@suiet/wallet-kit/style.css'
+import { WalletProvider } from '@suiet/wallet-kit'
 import type { AppProps } from 'next/app'
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <WalletProvider>
+      <Component {...pageProps} />
+    </WalletProvider>
+  )
 }
---
+++ src/components/WalletConnect.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import { ConnectButton } from '@suiet/wallet-kit';
+
+export const WalletConnect: React.FC = () => {
+  return (
+    <ConnectButton>Connect Wallet</ConnectButton>
+  )
+}
